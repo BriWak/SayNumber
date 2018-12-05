e80af59a3af675c45ec8268c471a738ca51f50c8
Brian Wakenshaw <brian.wakenshaw@digital.hmrc.gov.uk>
2018-12-05 08:51:07 +0000

    Working code and tests for any number up to plus or minus 1 trillion

---
+++ src/main/scala/SayNumber.scala
@@ -0,0 +1,73 @@
+
+object SayNumber extends App {
+
+  case class Negative(indicator: String, number: String)
+
+  def speak(number: String): String = {
+
+    val isNegative = handleNegative(number)
+
+    val numberByThousands = isNegative.number.reverse.grouped(3).toList.map(_.reverse)
+
+    val separators = List("", " Thousand ", " Million ", " Billion ")
+
+    val hundredsAsWords = numberByThousands.map(trebles)
+
+    isNegative.indicator + hundredsAsWords.zip(separators).reverse.flatMap(_.productIterator.toList).mkString("").trim
+  }
+
+  private def handleNegative(number: String): Negative = {
+    if (number.startsWith("-")) {
+      Negative("Minus ", number.tail)
+    } else {
+      Negative("", number)
+    }
+  }
+
+  private def singles(number: String): String = number match {
+      case "1" => "One"
+      case "2" => "Two"
+      case "3" => "Three"
+      case "4" => "Four"
+      case "5" => "Five"
+      case "6" => "Six"
+      case "7" => "Seven"
+      case "8" => "Eight"
+      case "9" => "Nine"
+      case _   => ""
+  }
+
+  private def doubles(number: String): String = number match {
+    case "10" => "Ten"
+    case "11" => "Eleven"
+    case "12" => "Twelve"
+    case "13" => "Thirteen"
+    case "14" => "Fourteen"
+    case "15" => "Fifteen"
+    case "16" => "Sixteen"
+    case "17" => "Seventeen"
+    case "18" => "Eighteen"
+    case "19" => "Nineteen"
+    case _ => number.head.toString match {
+      case "0" => singles(number.last.toString)
+      case "2" => "Twenty " + singles(number.last.toString)
+      case "3" => "Thirty " + singles(number.last.toString)
+      case "4" => "Forty " + singles(number.last.toString)
+      case "5" => "Fifty " + singles(number.last.toString)
+      case "6" => "Sixty " + singles(number.last.toString)
+      case "7" => "Seventy " + singles(number.last.toString)
+      case "8" => "Eighty " + singles(number.last.toString)
+      case "9" => "Ninety " + singles(number.last.toString)
+    }
+  }
+
+  private def trebles(number: String): String = number.length match {
+    case 1 if number == "0" => "Zero"
+    case 1 => singles(number)
+    case 2 => doubles(number)
+    case 3  if number.head.toString == "0" => doubles(number.tail)
+    case 3  if number.tail.toString == "00" => singles(number.head.toString) + " Hundred"
+    case _  => singles(number.head.toString) + " Hundred and " + doubles(number.tail)
+  }
+
+}
---
+++ src/test/scala/SayNumberSpec.scala
@@ -0,0 +1,97 @@
+import org.scalatest.{MustMatchers, WordSpec}
+
+class SayNumberSpec extends WordSpec with MustMatchers {
+
+  "speak" should {
+    "return 'Zero' when given 0" in {
+      SayNumber.speak("0") mustBe "Zero"
+    }
+
+    "return 'One' when given 1" in {
+      SayNumber.speak("1") mustBe "One"
+    }
+
+    "return 'Ten' when given 10" in {
+      SayNumber.speak("10") mustBe "Ten"
+    }
+
+    "return 'Eighteen' when given 18" in {
+      SayNumber.speak("18") mustBe "Eighteen"
+    }
+
+    "return 'Twenty' when given 20" in {
+      SayNumber.speak("20") mustBe "Twenty"
+    }
+
+    "return 'Twenty Seven' when given 27" in {
+      SayNumber.speak("27") mustBe "Twenty Seven"
+    }
+
+    "return 'Fifty Nine' when given 59" in {
+      SayNumber.speak("59") mustBe "Fifty Nine"
+    }
+
+    "return 'Ninety' when given 90" in {
+      SayNumber.speak("90") mustBe "Ninety"
+    }
+
+    "return 'Ninety Nine' when given 99" in {
+      SayNumber.speak("99") mustBe "Ninety Nine"
+    }
+
+    "return 'One Hundred' when given 100" in {
+      SayNumber.speak("100") mustBe "One Hundred"
+    }
+
+    "return 'One Hundred and Three' when given 103" in {
+      SayNumber.speak("103") mustBe "One Hundred and Three"
+    }
+
+    "return 'One Hundred and Ten' when given 110" in {
+      SayNumber.speak("110") mustBe "One Hundred and Ten"
+    }
+
+    "return 'One Hundred and Forty Seven' when given 147" in {
+      SayNumber.speak("147") mustBe "One Hundred and Forty Seven"
+    }
+
+    "return 'Eight Hundred and Eighty Eight' when given 888" in {
+      SayNumber.speak("888") mustBe "Eight Hundred and Eighty Eight"
+    }
+
+    "return 'One Thousand Two Hundred and Fifty Three' when given 1253" in {
+      SayNumber.speak("1253") mustBe "One Thousand Two Hundred and Fifty Three"
+    }
+
+    "return 'One Hundred and Seventy Six Million Nine Hundred Thousand Two Hundred and Fourteen' when given 176900214" in {
+      SayNumber.speak("176900214") mustBe "One Hundred and Seventy Six Million Nine Hundred Thousand Two Hundred and Fourteen"
+    }
+    "return 'Five Billion Six Hundred and Seventy Eight Million Nine Hundred and One Thousand Two Hundred and Three' when given 5678901203" in {
+      SayNumber.speak("5678901203") mustBe "Five Billion Six Hundred and Seventy Eight Million Nine Hundred and One Thousand Two Hundred and Three"
+    }
+
+    "return 'Minus One' when given -1" in {
+      SayNumber.speak("-1") mustBe "Minus One"
+    }
+
+    "return 'Minus Ten' when given -10" in {
+      SayNumber.speak("-10") mustBe "Minus Ten"
+    }
+
+    "return 'Minus Eighteen' when given -18" in {
+      SayNumber.speak("-18") mustBe "Minus Eighteen"
+    }
+
+    "return 'Minus Twenty' when given -20" in {
+      SayNumber.speak("-20") mustBe "Minus Twenty"
+    }
+
+    "return 'Minus Twenty Seven' when given -27" in {
+      SayNumber.speak("-27") mustBe "Minus Twenty Seven"
+    }
+
+    "return 'Minus Fifty Nine' when given -59" in {
+      SayNumber.speak("-59") mustBe "Minus Fifty Nine"
+    }
+  }
+}
